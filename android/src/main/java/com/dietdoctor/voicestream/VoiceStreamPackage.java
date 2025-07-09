373a0a0f1ce1f02b5f447ed016f7c9445e78ad6b
Omal Perera <omalperera8@gmail.com>
2025-07-09 14:00:09 +0200

    feat: add android autolink capability (#7)

--- android/src/main/java/com/dietdoctor/voicestream/VoiceStreamPackage.java
+++ android/src/main/java/com/dietdoctor/voicestream/VoiceStreamPackage.java
@@ -1,25 +1,40 @@
 package com.dietdoctor.voicestream;
 
-import com.facebook.react.ReactPackage;
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.module.model.ReactModuleInfo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 
-public class VoiceStreamPackage implements ReactPackage {
+public class VoiceStreamPackage extends TurboReactPackage {
     
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new VoiceStreamModule(reactContext));
-        return modules;
+    public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+        if (name.equals(VoiceStreamModule.NAME)) {
+            return new VoiceStreamModule(reactContext);
+        }
+        return null;
     }
 
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+    public ReactModuleInfoProvider getReactModuleInfoProvider() {
+        return () -> {
+            final HashMap<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+            moduleInfos.put(
+                VoiceStreamModule.NAME,
+                new ReactModuleInfo(
+                    VoiceStreamModule.NAME,
+                    VoiceStreamModule.NAME,
+                    false, // canOverrideExistingModule
+                    false, // needsEagerInit
+                    true,  // hasConstants
+                    false, // isCxxModule
+                    false  // isTurboModule (set to true if using new architecture)
+                )
+            );
+            return moduleInfos;
+        };
     }
 } 
