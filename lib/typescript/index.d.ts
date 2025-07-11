8787b7ff773e25b6c6bb17a57eea50d7ec910c74
Omal Perera <omalperera8@gmail.com>
2025-07-11 16:23:43 +0200

    chore: rename few functions in favour of understandability

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,5 +1,13 @@
-import { VoiceStreamerInterface } from './types';
+import { VoiceStreamerInterface } from "./types";
 declare const VoiceStream: VoiceStreamerInterface;
+/**
+ * Verifies whether microphone access is currently granted for this app
+ * @returns True if permission is granted, false otherwise
+ */
 export declare const checkMicrophonePermission: () => Promise<boolean>;
+/**
+ * Prompts the user to grant microphone access if not already authorized (Android only)
+ * @returns True if permission granted or already exists, false if denied
+ */
 export declare const requestMicrophonePermission: () => Promise<boolean>;
 export default VoiceStream;
